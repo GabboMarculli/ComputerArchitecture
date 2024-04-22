1fcf19fb0b1814a23df066ee60590a6929e3fedd
Fede-Russo <federico.russo.0513@gmail.com>
2024-04-22 18:44:07 +0200

    update

--- testScript.py
+++ testScript.py
@@ -1,25 +1,25 @@
 import subprocess
 
 # Path to the .exe program
-exe_path = "./k-means_array.exe"
+exe_path = "./k-medoids_parallelized_windows.exe"
 
 # File to redirect output to
-output_file = "Results_Array.csv"
+output_file = "results/Results_Medoids_windows_2.csv"
 
 nthread = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]
-npunti = ["2000", "20000", "200000", "2000000", "20000000"]
+npunti = ["500", "1000", "2500", "5000", "10000"]
+ncluster = ["3", "5", "10"]
 
-ntest = 16*5*36
+ntest = 16*5*3
 test = 1
 
 # Open the file for appending
 with open(output_file, "a") as file:
-    for t in nthread:
-        for p in npunti:
-            for i in range(36):
+    for p in npunti:
+        for c in ncluster:
+            for t in nthread:
                 # Command-line arguments
-                arguments = [p, t]
-
+                arguments = [p, "2", c, t]
                 # Launch the .exe program with arguments and redirect output to file
                 subprocess.run([exe_path] + arguments, stdout=file, stderr=subprocess.PIPE)
                 print("TEST COMPLETATI: [", test, "/", ntest,"]")
